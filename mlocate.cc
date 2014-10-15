2e3c4f335e54241ecce17f41d87c7d74eb798c10
Alexandre Detiste <alexandre.detiste@gmail.com>
2014-10-15 11:45:18 +0200

    trim mlocate.h

--- mlocate.cc
+++ mlocate.cc
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "mlocate_db.h"
+//       original filename is 'db.h'
+// TODO: should be packaged in /usr/include by 'mlocate' package
+//       like 'make' provides '/usr/include/gnumake.h'
+//       and  'sudo' provides '/usr/include/sudo_plugin.h'
+
 #include "mlocate.h"
 
 int read_mlocate(vector<string>& fs, vector<string>& prunefs)
--- mlocate.h
+++ mlocate.h
@@ -1,13 +1,5 @@
-#include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include "mlocate_db.h"
-//       original filename is 'db.h'
-// TODO: should be packaged in /usr/include by 'mlocate' package
-//       like 'make' provides '/usr/include/gnumake.h'
-//       and  'sudo' provides '/usr/include/sudo_plugin.h'
 
 using namespace std;
 
