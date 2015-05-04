931e717a4c4ff5c6b3d82603b7157f16406b0aa4
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-05-04 10:23:48 +0200

    Fix stack-smashing error, replace strcpy by strncpy (Closes: #783723)

--- cruft.cc
+++ cruft.cc
@@ -16,8 +16,6 @@
 #include "mlocate.h"
 #include "dpkg.h"
 
-#define debug false
-
 extern int shellexp(char* filename, char* pattern );
 
 using namespace std;
@@ -71,37 +69,45 @@ void updatedb()
 
 bool myglob(string file, string glob )
 {
+	bool debug = getenv("DEBUG") != NULL;
+
 	if (file==glob) return true;
 	unsigned int filesize=file.size();
 	unsigned int globsize=glob.size();
 		if ( glob.find("**")==globsize-2
 		  and filesize >= globsize-2
 		  and file.substr(0,globsize-2)==glob.substr(0,globsize-2)) {
-		//cout << "match ** " << file << " # " << glob << endl;
+		if (debug) cout << "match ** " << file << " # " << glob << endl;
 		return true;
 	}  else if ( glob.find("*")==globsize-1
 		  and filesize >= globsize-1
 		  and file.find("/",globsize-1)==string::npos
 		  and file.substr(0,globsize-1)==glob.substr(0,globsize-1)) {
-		//cout << "match * " << file << " # " << glob << endl;
+		if (debug) cout << "match * " << file << " # " << glob << endl;
 		return true;
 	} else if ( fnmatch(glob.c_str(),file.c_str(),FNM_PATHNAME)==0 ) {
-		//cout << "fnmatch " << file << " # " << glob << endl;
+		if (debug) cout << "fnmatch " << file << " # " << glob << endl;
 		return true;
 	} else {
 		// fallback to shellexp.c
-		char param1[128];
-		strcpy(param1,file.c_str());
-		char param2[128];
-		strcpy(param2,glob.c_str());
+		char param1[256];
+		strncpy(param1,file.c_str(),sizeof(param1));
+		param1[sizeof(param1)-1] = '\0';
+		char param2[256];
+		strncpy(param2,glob.c_str(),sizeof(param2));
+		param2[sizeof(param2)-1] = '\0';
 		bool result=shellexp(param1,param2);
-		//cout << result << ' ' << file << " # " << glob << endl;
+		if (result and debug) {
+			cout << "shellexp.c " << file << " # " << glob << endl;
+		}
 		return result;
 	}
 }
 
 int main(int argc, char *argv[])
 {
+	bool debug = getenv("DEBUG") != NULL;
+
 	if (argc > 1) {
 		cerr << "commandline arguments not yet implemented" << endl;
 		exit(1);
@@ -159,7 +165,7 @@ int main(int argc, char *argv[])
 	//fs.clear();
 	//dpkg.clear();
 
-	if (debug) cout << endl << missing.size() << " files in missing database" << endl;
+	if (debug) cout << missing.size() << " files in missing database" << endl;
 	if (debug) cout << cruft.size() << " files in cruft database" << endl << endl << flush;
 
 	// TODO: this should use DPKG database too
