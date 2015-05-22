f82418df77fc13d56b8465acc6268a9e65a88837
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-05-22 07:32:36 +0200

    process scripts in /etc/cruft/explain/ too !

--- explain.cc
+++ explain.cc
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include "explain.h"
 
 void read_one_explain(const string& script,vector<string>& explain)
@@ -28,41 +29,63 @@ int read_explain(vector<string>& packages, vector<string>& explain)
 {
 	bool debug=getenv("DEBUG") != NULL;
 
-	if (debug) cerr << "EXECUTING FILTERS IN /usr/lib/cruft/explain/" << endl;
-
+	if (debug) cerr << "EXECUTING UPPERCASE FILTERS IN /usr/lib/cruft/explain/" << endl;
 	DIR *dp;
 	struct dirent *dirp;
 	if((dp = opendir("/usr/lib/cruft/explain/")) == NULL) {
 	      cerr << "Error(" << errno << ") opening /usr/lib/cruft/explain/" << endl;
 	      return errno;
 	}
-
 	while ((dirp = readdir(dp)) != NULL) {
 		string package=string(dirp->d_name);
 		if (package==".") continue;
 		if (package=="..") continue;
-		if (package=="dpkg") continue; /* this is done in read_dpkg_items() */
-		if (package=="USERS") continue; /* replaced by USERS_cruft-ng */
-		bool match=false;
+		if (package=="USERS") continue; // replaced by USERS_cruft-ng
+		string uppercase=package;
+		transform(uppercase.begin(), uppercase.end(), uppercase.begin(), ::toupper);
+		if (package==uppercase or package=="USERS_cruft-ng")
+			read_one_explain("/usr/lib/cruft/explain/" + package, explain);
+	}
+	if (debug) cerr << endl;
+
+	if (debug) cerr << "EXECUTING UPPERCASE FILTERS IN /etc/cruft/explain/" << endl;
+	if((dp = opendir("/etc/cruft/explain/")) == NULL) {
+	      cerr << "Error(" << errno << ") opening /etc/cruft/explain/" << endl;
+	      return errno;
+	}
+	while ((dirp = readdir(dp)) != NULL) {
+		string package=string(dirp->d_name);
+		if (package==".") continue;
+		if (package=="..") continue;
 		string uppercase=package;
 		transform(uppercase.begin(), uppercase.end(), uppercase.begin(), ::toupper);
 		if (package==uppercase)
-			match=true;
-		if (package=="USERS_cruft-ng")
-			match=true;
-		else {
-			vector<string>::iterator it=packages.begin();
-			for (;it !=packages.end();it++) {
-			      if (package==*it) {
-				    match=true;
-				    break;
-			      }
-			}
-		}
-		if (debug) cerr << match << ' ' << package << endl;
-		if (match) read_one_explain("/usr/lib/cruft/explain/" + package, explain);
+			read_one_explain("/etc/cruft/explain/" + package, explain);
 	}
-	closedir(dp);
+	if (debug) cerr << endl;
+
+
+	if (debug) cerr << "EXECUTING OTHER FILTERS" << endl;
+	vector<string>::iterator it=packages.begin();
+
+	string retain;
+	for (;it !=packages.end();it++) {
+                string package=*it;
+                size_t arch=package.find(":");
+                if (arch != string::npos ) package=package.substr(0,arch);
+                if (package==retain) continue;
+                retain=package;
+
+		if (package=="dpkg") continue; /* this is done in read_dpkg_items() */
+
+                struct stat stat_buffer;
+                string etc_filename = "/etc/cruft/explain/" + package;
+                string usr_filename = "/usr/lib/cruft/explain/" + package;
+                if ( stat(etc_filename.c_str(), &stat_buffer)==0 )
+                        read_one_explain(etc_filename, explain);
+                else if ( stat(usr_filename.c_str(), &stat_buffer)==0 )
+                        read_one_explain(usr_filename, explain);
+	}
 	sort(explain.begin(), explain.end());
 	return 0;
 }
