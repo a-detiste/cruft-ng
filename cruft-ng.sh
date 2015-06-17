044b5886a939c8100c5951d18942d309c60eddd9
Alexandre Detiste <alexandre.detiste@gmail.com>
2015-06-17 08:59:34 +0200

    regression test: prefer in-tree pgm if available

--- cruft-ng.sh
+++ cruft-ng.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 [ -e "/home/$USER/cruft-ng.log" ] && mv "/home/$USER/cruft-ng.log" "/home/$USER/cruft-ng.log.old"
-sudo cruft-ng > "/home/$USER/cruft-ng.log"
-ls -l "/home/$USER/cruft-ng.log*"
+if [ -x cruft-ng ]
+then
+	sudo ./cruft-ng > "/home/$USER/cruft-ng.log"
+else
+	sudo cruft-ng > "/home/$USER/cruft-ng.log"
+fi
+ls -l /home/$USER/cruft-ng.log*
 [ -e "/home/$USER/cruft-ng.log.old" ] && colordiff -u "/home/$USER/cruft-ng.log.old" "/home/$USER/cruft-ng.log"
