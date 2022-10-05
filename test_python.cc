a0ed0a556abf8a0f56f9d54e5141cc2b43c13bb7
Alexandre Detiste <alexandre.detiste@gmail.com>
2022-10-05 23:30:56 +0200

    python: more tests

--- test_python.cc
+++ test_python.cc
@@ -5,20 +5,26 @@ int main(int argc, char *argv[])
 {
 	if (pyc_has_py("/usr/share/games/game-data-packager/game_data_packager/__pycache__")) {
 		cout << "OK" << endl;
-        } else {
+	} else {
 		cout << "ERROR" << endl;
-        };
+	};
 
 	// my last Python2 file
-        if (pyc_has_py("/home/tchet/git/pyposdisplay/pyposdisplay/pyposdisplay.pyc")) {
+	if (pyc_has_py("/home/tchet/git/pyposdisplay/pyposdisplay/pyposdisplay.pyc")) {
 		cout << "OK" << endl;
-        } else {
+	} else {
 		cout << "ERROR" << endl;
-        };
+	};
 
 	if (pyc_has_py("/usr/share/python3/debpython/__pycache__/option.cpython-310.pyc")) {
 		cout << "OK" << endl;
-        } else {
+	} else {
 		cout << "ERROR" << endl;
-        };
+	};
+
+	if (!pyc_has_py("/home/tchet/git/pyposdisplay/pyposdisplay/pyposdisplay.py")) {
+		cout << "OK" << endl;
+	} else {
+		cout << "ERROR" << endl;
+	};
 }
