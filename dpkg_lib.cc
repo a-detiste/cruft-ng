534832ac1a222f023960f778abe0dd2a028242f0
Alexandre Detiste <alexandre.detiste@gmail.com>
2022-10-08 00:17:04 +0200

    WIP with C library

--- dpkg_lib.cc
+++ dpkg_lib.cc
@@ -1,22 +1,72 @@
 #define LIBDPKG_VOLATILE_API
 #include <iostream>
+#include <algorithm>
 
 #include <dpkg/dpkg.h>
 #include <dpkg/dpkg-db.h>
+#include <dpkg/db-fsys.h>
 #include <dpkg/pkg-array.h>
+#include <dpkg/pkg-show.h>
 #include "dpkg.h"
+
+/*
+https://www.dpkg.org/doc/libdpkg/structpkginfo.html
+*/
 
 int read_dpkg_header(vector<string>& packages)
 {
+	return 0; // doing dpkg_program_init() will later lead to coredump
+	int i;
 	struct pkg_array array;
+	struct pkginfo *pkg;
+
 	dpkg_program_init("cruft");
 	modstatdb_open(msdbrw_available_readonly);
 	pkg_array_init_from_hash(&array);
-	cout << "packages: " << array.n_pkgs << endl;
+	pkg_array_sort(&array, pkg_sorter_by_nonambig_name_arch);
+	for (i = 0; i < array.n_pkgs; i++) {
+		pkg = array.pkgs[i];
+		if (pkg->status == PKG_STAT_INSTALLED) {
+			packages.push_back(pkg->set->name);
+		}
+	}
+	modstatdb_shutdown();
 	dpkg_program_done();
-	return -1;
+	return 0;
 }
-int read_dpkg_items(vector<string>& dpkg)
+
+int read_dpkg_items(vector<string>& output)
 {
-	return -1;
+	int i;
+	struct pkg_array array;
+	struct pkginfo *pkg;
+	struct fsys_namenode_list *file;
+	struct fsys_namenode *namenode;
+	bool debug = getenv("DEBUG") != NULL;
+
+	dpkg_program_init("cruft");
+	modstatdb_open(msdbrw_readonly);
+	pkg_array_init_from_hash(&array);
+	pkg_array_sort(&array, pkg_sorter_by_nonambig_name_arch);
+
+	for (i = 0; i < array.n_pkgs; i++) {
+		pkg = array.pkgs[i];
+		if (pkg->status == PKG_STAT_INSTALLED) {
+			if (debug) cout << "PACKAGE:" <<pkg->set->name << endl;
+			ensure_packagefiles_available(pkg);
+			ensure_diversions();
+			file = pkg->files;
+			while (file) {
+				namenode = file->namenode;
+				if (debug) cout << namenode->name << endl;
+				output.push_back(namenode->name);
+				file = file->next;
+		        }
+		}
+	}
+	modstatdb_shutdown();
+	dpkg_program_done();
+	sort(output.begin(), output.end());
+	output.erase( unique( output.begin(), output.end() ), output.end() );
+	return 0;
 }
--- test_dpkg.cc
+++ test_dpkg.cc
@@ -6,6 +6,11 @@ int main(int argc, char *argv[])
 	vector<string> packages;
 	read_dpkg_header(packages);
 
+	for (unsigned int i=0;i<packages.size();i++) {
+		cout << packages[i] << endl;
+	}
+	cout << endl;
+
 	vector<string> db;
 	read_dpkg_items(db);
 
