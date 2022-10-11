2cf752bf5141c0d4709c070ebc1098768ffb0d5d
Alexandre Detiste <alexandre.detiste@gmail.com>
2022-10-11 23:31:27 +0200

    workaround plocate's PRUNEPATH=/var/splool

--- plocate.cc
+++ plocate.cc
@@ -17,12 +17,19 @@ int read_plocate(vector<string>& fs, vector<string>& prunefs)
 	string line;
 
 	if (debug) cerr << "PLOCATE DATA\n";
+
+	fs.push_back("/.");
+	fs.push_back("/dev");
+	fs.push_back("/home");
+	fs.push_back("/root");
+	fs.push_back("/tmp");
+
 	string command = "plocate /";
-        char buf[PATH_MAX];
-        FILE* fp;
-        if ((fp = popen(command.c_str(), "r")) == NULL) return 1;
-        while (fgets(buf, sizeof(buf),fp))
-        {
+	char buf[PATH_MAX];
+	FILE* fp;
+	if ((fp = popen(command.c_str(), "r")) == NULL) return 1;
+	while (fgets(buf, sizeof(buf),fp))
+	{
 		buf[strlen(buf)-1] = '\0';
 		string filename = buf;
 		string toplevel = filename.substr(0, filename.find("/", 1));
@@ -35,7 +42,20 @@ int read_plocate(vector<string>& fs, vector<string>& prunefs)
 		if (!pyc_has_py(filename, false)) fs.push_back(filename);
 	}
 	pclose(fp);
+
+	// default PRUNEPATH in /etc/updatedb.conf
+	command = "find /var/spool";
+	if ((fp = popen(command.c_str(), "r")) == NULL) return 1;
+	while (fgets(buf, sizeof(buf),fp))
+	{
+		buf[strlen(buf)-1] = '\0';
+		string filename = buf;
+		fs.push_back(filename);
+	}
+	pclose(fp);
+
 	sort(fs.begin(), fs.end());
+	fs.erase( unique( fs.begin(), fs.end() ), fs.end() );
 	if (debug) cerr << fs.size() << " relevant files in PLOCATE database"  << endl << endl;
 	return 0;
 }
