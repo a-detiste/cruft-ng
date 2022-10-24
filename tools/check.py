b53febfe3e7603fae6727210322723d52c7dd6da
Alexandre Detiste <alexandre.detiste@gmail.com>
2022-10-24 07:24:26 +0200

    automatic archive of old rules

--- tools/check.py
+++ tools/check.py
@@ -4,19 +4,23 @@ import glob
 import os
 import subprocess
 
-current_packages = set()
+import distro_info
+
+STABLE = distro_info.DebianDistroInfo().stable()
+
+testing = set()
 proc = subprocess.Popen(['apt-cache', 'pkgnames'],
                         universal_newlines=True,
                         stdout=subprocess.PIPE)
 for line in proc.stdout:
-    current_packages.add(line.rstrip())
+    testing.add(line.rstrip())
 
 # Add RaspBian
-current_packages.add('raspberrypi-bootloader')
-current_packages.add('libraspberrypi0')
+testing.add('raspberrypi-bootloader')
+testing.add('libraspberrypi0')
 
 # Google repository
-current_packages.add('google-earth-stable')
+testing.add('google-earth-stable')
 
 # Add Stable, remove cache file to trigger download
 if not os.path.isfile('tools/Packages_amd64'):
@@ -26,17 +30,29 @@ if not os.path.isfile('tools/Packages_amd64'):
                           cwd='tools')
     os.unlink('tools/Sources')
 
+stable = set()
 with open('tools/Packages_amd64', 'r', encoding='utf8') as p:
     for line in p:
         if line.startswith('Package:'):
-            current_packages.add(line.split(':', 1)[1].strip())
+            stable.add(line.split(':', 1)[1].strip())
 
 
 filters = set([os.path.basename(f) for f in glob.glob('rules/*')])
 explain = set([os.path.basename(f) for f in glob.glob('explain/*') if f == f.lower()])
 
-unknown_filters = filters - current_packages
-print('Filters for unknown packages', sorted(unknown_filters))
+def process(category, packages, testing, stable, archive):
+    unknown = packages - testing
+    deprecated = unknown & stable
+    unknown = unknown - stable
+    print('[%s]:' % category)
+    print('moved to %s:' % STABLE, sorted(deprecated))
+    for item in deprecated:
+        destdir = os.path.join('archive', archive)
+        if not os.path.isdir(destdir):
+            os.makedirs(destdir)
+        subprocess.call(['git', 'mv', os.path.join(category, item), destdir])
+    print('unknown:', sorted(unknown))
 
-unknown_explain = explain - current_packages
-print('Explain scripts for unknown packages', sorted(unknown_explain))
+process('rules', filters, testing, stable, STABLE)
+print()
+process('explain', explain, testing, stable, 'explain')
