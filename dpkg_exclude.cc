c7cc15ec3e11cfa1af3ec9d0cc09bcd0b27c0887
Christian Göttsche <cgzones@googlemail.com>
2022-10-30 23:17:45 +0100

    dpkg_exclude: update
    - avoid string copies
    - align parameter names in declaration and definition of
      read_dpkg_excludes()

--- dpkg_exclude.cc
+++ dpkg_exclude.cc
@@ -4,38 +4,35 @@
 
 #include "dpkg_exclude.h"
 
-int read_one_cfg(const string& filename, vector <string>& globs)
+static int read_one_cfg(const string& filename, vector <string>& globs)
 {
-	ifstream glob_file(filename.c_str());
-	while (glob_file.good())
+	ifstream glob_file(filename);
+	for (string glob_line; getline(glob_file,glob_line);)
 	{
-		string glob_line;
-		getline(glob_file,glob_line);
-		if (glob_file.eof()) break;
 		if (glob_line.find("path-exclude") != 0) continue;
 		glob_line = glob_line.substr(glob_line.find('/'));
                 // translate regular globs into cruft ones
-		if (glob_line.substr(glob_line.length()-1,1) == "*")
+		if (glob_line.back() == '*')
 			glob_line += '*';
-		globs.push_back(glob_line);
+		globs.emplace_back(glob_line);
 	}
 	return 0;
 }
 
-int read_dpkg_excludes(vector<string>& globs)
+int read_dpkg_excludes(vector<string>& excludes)
 {
-	read_one_cfg("/etc/dpkg/dpkg.cfg", globs);
+	read_one_cfg("/etc/dpkg/dpkg.cfg", excludes);
 
 	DIR *dp;
 	struct dirent *dirp;
-	if((dp = opendir("/etc/dpkg/dpkg.cfg.d/")) == NULL) {
+	if((dp = opendir("/etc/dpkg/dpkg.cfg.d/")) == nullptr) {
 	      cerr << "Error(" << errno << ") opening /etc/dpkg/dpkg.cfg.d/" << endl;
 	      return 1;
 	}
-	while ((dirp = readdir(dp)) != NULL) {
-		string cfg = string(dirp->d_name);
+	while ((dirp = readdir(dp)) != nullptr) {
+		string cfg = dirp->d_name;
 		if (cfg == "." or cfg == "..") continue;
-		read_one_cfg("/etc/dpkg/dpkg.cfg.d/" + cfg, globs);
+		read_one_cfg("/etc/dpkg/dpkg.cfg.d/" + cfg, excludes);
 	}
 	closedir(dp);
 	return 0;
