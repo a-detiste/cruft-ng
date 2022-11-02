2bfb10c16b5353f6b2429114bf0abf87351438da
Christian Göttsche <cgzones@googlemail.com>
2022-11-02 20:56:20 +0100

    usr_merge: handle uncommon usr-merge locations

--- usr_merge.cc
+++ usr_merge.cc
@@ -19,12 +19,15 @@ static bool check_link(const string& path, const bool mandatory)
 string usr_merge(const string& path)
 {
 	static bool setup = false;
-	static bool MERGED_BIN, MERGED_LIB, MERGED_LIB32, MERGED_SBIN;
+	static bool MERGED_BIN, MERGED_LIB, MERGED_LIB32, MERGED_LIB64, MERGED_LIBO32, MERGED_LIBX32, MERGED_SBIN;
 	if (!setup)
 	{
 		MERGED_BIN=check_link("/bin", true);
 		MERGED_LIB=check_link("/lib", true);
 		MERGED_LIB32=check_link("/lib32", false);
+		MERGED_LIB64=check_link("/lib64", false);
+		MERGED_LIBO32=check_link("/libo32", false);
+		MERGED_LIBX32=check_link("/libx32", false);
 		MERGED_SBIN=check_link("/sbin", true);
 		setup=true;
 	}
@@ -32,6 +35,9 @@ string usr_merge(const string& path)
 	if ((MERGED_BIN and path.rfind("/bin/", 0) == 0)
 	 or (MERGED_LIB and path.rfind("/lib/", 0) == 0)
 	 or (MERGED_LIB32 and path.rfind("/lib32/", 0) == 0)
+	 or (MERGED_LIB64 and path.rfind("/lib64/", 0) == 0)
+	 or (MERGED_LIBO32 and path.rfind("/libo32/", 0) == 0)
+	 or (MERGED_LIBX32 and path.rfind("/libx32/", 0) == 0)
 	 or (MERGED_SBIN and path.rfind("/sbin/", 0) == 0))
 		return "/usr" + path;
 	else
