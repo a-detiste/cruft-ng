babd36d8c2f205c43371cd1fb2ee3034a56133f4
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-08-12 18:08:47 +0200

    python: refresh the unit test

--- test_python.cc
+++ test_python.cc
@@ -1,24 +1,20 @@
 #include <iostream>
 #include "python.h"
 
-void test(string path) {
+void test(string path, bool expected) {
 	cout << "checking: " << path << endl;
-	if (pyc_has_py(path, true)) {
-		cout << "OK" << endl;
+	if (pyc_has_py(path, true) == expected) {
+		cout << "\033[1;32mOK\033[0m" << endl << endl;
 	} else {
-		cout << "ERROR" << endl;
+		cout << "\033[1;31mERROR\033[0m" << endl << endl;
 	};
 }
 
 int main()
 {
-	test("/usr/share/games/game-data-packager/game_data_packager/__pycache__");
-	test("/home/tchet/git/pyposdisplay/pyposdisplay/pyposdisplay.pyc");
-	test("/usr/share/python3/debpython/__pycache__/option.cpython-310.pyc");
-
-	if (!pyc_has_py("/home/tchet/git/pyposdisplay/pyposdisplay/pyposdisplay.py", true)) {
-		cout << "OK" << endl;
-	} else {
-		cout << "ERROR" << endl;
-	};
+	test("/usr/share/games/game-data-packager/game_data_packager/__pycache__", true);
+	test("/usr/share/python3/debpython/__pycache__/option.cpython-310.pyc", false);
+	test("/usr/share/python3/debpython/__pycache__/option.cpython-311.pyc", true);
+	test("/usr/share/python3/debpython/__pycache__/option.cpython-312.pyc", true);
+	test("/usr/share/python3/debpython/__pycache__/option.cpython-313.pyc", false);
 }
