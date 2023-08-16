35fff305cb608d6d867dfbb2dee5ab100d651c72
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-08-16 16:20:41 +0200

    testing explain scripts

---
+++ smoke_test_explain.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -u
+
+# all scripts should run without failure or printing to stderr,
+# even if the matching package is not installed
+
+OK="\033[1;32mOK\033[0m"
+KO="\033[1;31mKO\033[0m"
+
+for script in explain/*
+do
+   echo -n "$script "
+   $script > /dev/null && echo "$OK" || echo "$KO"
+done
