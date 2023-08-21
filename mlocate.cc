6555c6e9e6f4d9cfd14a1fd9127c0b9774269e3c
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-08-22 01:58:09 +0200

    mlocate: remove a TODO that was done for a brief time

--- mlocate.cc
+++ mlocate.cc
@@ -7,9 +7,6 @@
 #include <limits>
 #include "mlocate_db.h"
 //       original filename is 'db.h'
-// TODO: should be packaged in /usr/include by 'mlocate' package
-//       like 'make' provides '/usr/include/gnumake.h'
-//       and  'sudo' provides '/usr/include/sudo_plugin.h'
 
 #include "locate.h"
 #include "python.h"
