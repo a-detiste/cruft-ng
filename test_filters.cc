65703de7c045f6a5021a4ca4bf06ab71fe640d6f
Alexandre Detiste <alexandre.detiste@gmail.com>
2024-05-04 21:28:19 +0200

    refresh test

--- test_filters.cc
+++ test_filters.cc
@@ -5,9 +5,11 @@
 
 int main()
 {
-	vector<string> packages,useless;
+	vector<string> packages;
 	vector<owner> globs;
-	read_dpkg(packages, useless, false);
+	dpkg_start();
+	read_dpkg_header(packages);
+	dpkg_end();
 	read_filters("/etc/cruft/filters/", "/usr/share/cruft/ruleset", packages, globs);
 	for (unsigned int i=0;i<globs.size();i++) {
 		cout << globs[i].package << ' ' << globs[i].path << endl;
