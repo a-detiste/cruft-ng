69a43bbeb527a095e37411713cf9ae8b7f1d6a07
Alexandre Detiste <alexandre.detiste@gmail.com>
2025-09-07 13:45:18 +0200

    drop the old transitional package & link

--- cruft-ng.sh
+++ cruft-ng.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 [ -e "/home/$USER/cruft-ng.log" ] && mv "/home/$USER/cruft-ng.log" "/home/$USER/cruft-ng.log.old"
-if [ -x cruft-ng ]
+if [ -x cruft ]
 then
-	sudo ./cruft-ng > "/home/$USER/cruft-ng.log"
+	sudo ./cruft > "/home/$USER/cruft-ng.log"
 else
-	sudo cruft-ng > "/home/$USER/cruft-ng.log"
+	sudo cruft > "/home/$USER/cruft-ng.log"
 fi
 ls -l /home/$USER/cruft-ng.log*
 [ -e "/home/$USER/cruft-ng.log.old" ] && colordiff -u "/home/$USER/cruft-ng.log.old" "/home/$USER/cruft-ng.log"
