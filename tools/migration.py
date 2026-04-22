d77ed468b374cb47a1bf252ccd5f967ed1374436
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-04-22 13:56:10 +0200

    fix migration.py

--- tools/migration.py
+++ tools/migration.py
@@ -6,10 +6,12 @@ import glob
 import os
 import subprocess
 
+STABLE = os.path.basename(os.readlink('archive/stable'))
+
 for rule in glob.glob('rules/*'):
     package = os.path.basename(rule)
     dest = os.path.join(BASE, package)
     if os.path.isfile(dest):
         print('%s has been dh-cruft enabled' % package)
         subprocess.call(['diff', '-u', rule, dest])
-        subprocess.call(['git', 'mv', rule, 'archive/stable'])
+        subprocess.call(['git', 'mv', rule, 'archive/%s/%s' % (STABLE, package)])
