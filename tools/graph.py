8ab847b9ffc58090e012e353f8ed370e98807e0b
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-04-26 17:00:18 +0200

    graph.py: go back to the start

--- tools/graph.py
+++ tools/graph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import datetime
+from datetime import datetime
 import os
 
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ import matplotlib.ticker as mtick
 
 from git import Repo
 
-def graph(package: str, rules: str) -> tuple[list[datetime.datetime],list[int]]:
+def graph(package: str, rules: str) -> tuple[list[datetime],list[int]]:
     repo = Repo.init('/home/tchet/deb/%s' % package)
     count = dict()
 
@@ -20,7 +20,7 @@ def graph(package: str, rules: str) -> tuple[list[datetime.datetime],list[int]]:
     stamps = []
     values = []
     for k, v in sorted(count.items()):
-        stamps.append(datetime.datetime.fromtimestamp(k))
+        stamps.append(datetime.fromtimestamp(k))
         values.append(v)
     return stamps, values
 
@@ -28,9 +28,17 @@ st_old, c_old = graph('cruft', 'filters-unex')
 st_new, c_new = graph('cruft-ng', 'rules')
 
 # split cruft <-> cruft-common
-CUTOFF = datetime.datetime(2015, 6, 29)
+SEED: dict[datetime, int] = {
+    datetime(1998, 4, 10): 81, # https://lists.debian.org/debian-policy/1998/04/msg00089.html
+}
+
+CUTOFF = datetime(2015, 6, 29)
 st_cruft = list()
 c_cruft = list()
+for k, v in sorted(SEED.items()):
+    st_cruft.append(k)
+    c_cruft.append(v)
+
 st_common = list()
 c_common = list()
 for st, c in zip(st_old, c_old):
