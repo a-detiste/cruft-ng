7287e8f9accabd6cd5e7269a9e27e60a03419764
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-05-12 19:01:10 +0200

    tools/codesearch.py do not mixup source & binary packages

--- tools/codesearch.py
+++ tools/codesearch.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 
+PATH = '/var/cache'
+
 import os
 import subprocess
 
-PATH = '/var/cache'
+import apt
+CACHE = apt.Cache()
+
+def bin2src(binaries: list[str]) -> list[str]:
+    '''compare source packages with source packages'''
+    sources = []
+    for binary in binaries:
+        try:
+            package = CACHE[binary]
+            source = package.candidate.source_name
+        except KeyError:
+            #print('!!! %s' % binary) # does not exists in Debian
+            source = binary
+        sources.append(source)
+    return sources
 
 done = subprocess.check_output(['apt-file', 'search', '--package-only', '/usr/share/cruft/rules/'], text=True).splitlines()
 pending = os.listdir('rules/')
+pending = [p for p in sorted(pending) if p.lower() == p]  # ignore DH_CRUFT & DEBOOTSTRAP
 todo = subprocess.check_output(['codesearch', 'path:postrm %s' % PATH, '--only'], text=True).splitlines()
 
-# BUG: mix binary vs source package
+done = bin2src(done)
+pending = bin2src(pending)
 to_check = set(todo) - set(done) - set(pending)
 print('\n'.join(sorted(to_check)))
