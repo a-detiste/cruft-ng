537d53512346b774605cdfeccbd34996c0e2668a
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-05-15 10:05:59 +0200

    codesearch.py: accept $1 parameter

--- tools/codesearch.py
+++ tools/codesearch.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
-PATH = '/var/lib'
-
 import os
 import subprocess
+import sys
+
+try:
+   PATH = sys.argv[1]
+   assert PATH in ('cache', 'games', 'lib', 'log', 'spool')
+except:
+   exit('tools/codesearch.py cache|games|lib|log|spool')
 
 import apt
 CACHE = apt.Cache()
@@ -24,7 +29,7 @@ def bin2src(binaries: list[str]) -> list[str]:
 done = subprocess.check_output(['apt-file', 'search', '--package-only', '/usr/share/cruft/rules/'], text=True).splitlines()
 pending = os.listdir('rules/')
 pending = [p for p in sorted(pending) if p.lower() == p]  # ignore DH_CRUFT & DEBOOTSTRAP
-todo = subprocess.check_output(['codesearch', 'path:postrm %s' % PATH, '--only', '--max-result', '500'], text=True).splitlines()
+todo = subprocess.check_output(['codesearch', 'path:postrm /var/%s' % PATH, '--only', '--max-result', '500'], text=True).splitlines()
 
 done = bin2src(done)
 pending = bin2src(pending)
